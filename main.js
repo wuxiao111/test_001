17c6bfaeda25424202b3c2a23cd7cc6368208e5e
liyuanjun <980500807@qq.com>
2018-05-09 18:21:21 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// const fibonacci = require('./lib/fibonacci')
+// console.log(fibonacci(1));
+const fibonacci=require('./lib/fibonacci')
+
+for(var i=1;i<20;i++){
+    console.log(fibonacci(i));
+}
